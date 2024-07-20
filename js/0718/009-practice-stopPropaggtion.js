f6a38cbcc516c23ef2f62599d56dec5523a30117
Forhye <onlyh4425@gmail.com>
2024-07-20 23:45:54 +0900

    ✨ 투두리스트 리스트 삭제 시 리스트 넘버 조절 연구중

--- js/0718/009-practice-stopPropaggtion.js
+++ js/0718/009-practice-stopPropaggtion.js
@@ -12,34 +12,62 @@ const $todoBtn = document.getElementsByClassName("todoBtn")[0];
 const $input = document.querySelector("input");
 const $mokdata = document.querySelector(".mokdata");
 let index = document.querySelectorAll(".mokdata p").length;
-console.log(index);
 
 $todoBtn.addEventListener("click", function () {
   let newP = document.createElement("p");
   let newBtn = document.createElement("button");
+  let createIndex = document.createElement("span");
 
   newBtn.innerText = "삭제";
   newBtn.className = "delBtn";
-  newP.innerText = `${++index}. ${$input.value}`;
 
+  newP.setAttribute("listNum", index); // index를 listNum에 넣는다
+
+  createIndex.className = "index";
+  createIndex.innerText = `${index}`;
+
+  newP.appendChild(createIndex);
+  newP.innerText = $input.value;
   newP.appendChild(newBtn);
   $mokdata.appendChild(newP);
 });
 
-function indexSort() {
-  document.querySelectorAll(".mokdata p");
-}
-indexSort();
-
 $mokdata.addEventListener("click", function (e) {
   if (e.target.nodeName === "P") {
     alert(e.target.innerText.slice(0, -2));
+    // e.target.texContent.replace(btn.textContent, "") // innerText로는 안됨
+    // trim() 공백을 없애줌
+    // trim은 사용자로부터 받은 값을 뭔갈 검증(벨리데이션)하거나 확인해야할 때 꼭 써주기
   } else if (e.target.nodeName === "BUTTON") {
+    let getIndex = e.target.parentNode.getAttribute("listNum");
     confirm("정말 삭제하시겠습니까?")
-      ? (e.target.parentNode.remove(), alert("삭제되었습니다."))
+      ? (e.target.parentNode.remove(),
+        alert("삭제되었습니다."),
+        updateIndex(getIndex))
       : alert("취소되었습니다.");
   }
 });
+
+// 6번을 삭제 했을 때 num = 6
+
+function updateIndex(num) {
+  // 전체 p의 갯수를 다시 구한다
+  let $p = document.querySelectorAll(".mokdata p");
+  $p.forEach((item) => {
+    let index = parseInt(item.getAttribute("listNum"));
+    if (index > num) {
+      item.querySelector();
+      const newIndex = index - 1;
+      item.setAttribute("listNum", newIndex);
+      console.log(typeof index);
+      console.log(newIndex);
+    }
+  });
+}
+
+// 삭제되었을 때.
+// 삭제된 타겟의 P의 형제 다음 요소를 불러오고 (다음 요소 이후는 변하지 않음)
+// P의 형제요소의 index를 찾아서 -1을 해준다
 
 // // p태그를 순회하면서 클릭 이벤트 발생
 // const allElement = $p.forEach((item) => {
