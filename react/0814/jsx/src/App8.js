7a14ffed222a9f2460420ae7aaf4b7b3a796d14a
Forhye <onlyh4425@gmail.com>
2024-08-14 16:17:46 +0900

    ✅ add : ColorText 컴포넌트 생성 후 불러와서 활용하기

---
+++ react/0814/jsx/src/App8.js
@@ -0,0 +1,13 @@
+import ColorText from "./components/ColorText";
+
+function App8() {
+  return (
+    <div>
+      <ColorText color="tomato" />
+      <ColorText color="darkolivegreen" />
+      <ColorText color="royalblue" />
+    </div>
+  );
+}
+
+export default App8;
---
+++ react/0814/jsx/src/components/ColorText.jsx
@@ -0,0 +1,9 @@
+function ColorText(color) {
+  return (
+    <>
+      <h1 style={color}>색이 바뀌어요!</h1>
+    </>
+  );
+}
+
+export default ColorText;
