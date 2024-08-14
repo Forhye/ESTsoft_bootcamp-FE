05e74b85e717f277bad2ce22025b855f2c4a0940
Forhye <onlyh4425@gmail.com>
2024-08-14 11:03:23 +0900

    ✅ update : js를 활용하여 날짜와 시간을 보여주기 실습 + 12시간제 표현법 + 오전 오후 표현법 + 분이 1의 자리 일 경우 앞에 0을 붙여주기

--- react/0814/jsx/src/App.js
+++ react/0814/jsx/src/App.js
@@ -1,11 +1,39 @@
 import "./App.css";
 
 function App() {
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+  // const time = today.getTime();
+  const time = today.toLocaleTimeString();
+
+  // const hour = today.getHours();
+  // const min = today.getMinutes();
+  // const sec = today.getSeconds();
+
+  // 현업에서 사용, 시간을 24시간제, 12시간제 표현할때
+  let hour = today.getHours() > 12 ? today.getHours() - 12 : today.getHours();
+
+  // 오전, 오후인지 판단
+  const ampm = today.getHours() > 12 ? "오후" : "오전";
+
+  // 분이 한자리 일때 앞에 0을 붙이지
+  const minutes =
+    today.getMinutes() > 10 ? today.getMinutes : "0" + today.getMinutes();
+
   return (
-    <div className="newClass">
-      <h1>안녕, 라이캣 1호</h1>
-      <h2>안녕, 라이캣 2호!</h2>
-      <div className="newClass" /> {/* class대신 className=""로 진행! */}
+    <div style={{ background: "#000", color: "white" }}>
+      <p className="red">년 : {year}</p>
+      <p>
+        월/일 : {month}/{day}
+      </p>
+      <p>시간 : {time}</p>
+      <p>
+        12시간제 : {ampm} {hour}시 {minutes}
+      </p>
+      {/* <h1>시간 : {hour}시 {min}분 {sec}초</h1> */}
     </div>
   );
 }
