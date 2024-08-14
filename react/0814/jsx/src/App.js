b23e60f8a10174db94dc1f37579cdb440baad7a3
Forhye <onlyh4425@gmail.com>
2024-08-14 10:09:14 +0900

    ✅ update : 스타일링 및 스타일 연결하는 법

--- react/0814/jsx/src/App.js
+++ react/0814/jsx/src/App.js
@@ -1,8 +1,13 @@
+import "./App.css";
+
 function App() {
   return (
-    <div>
-      hello world
+    <div className="newClass">
+      <h1>안녕, 라이캣 1호</h1>
+      <h2>안녕, 라이캣 2호!</h2>
+      <div className="newClass" /> {/* class대신 className=""로 진행! */}
     </div>
   );
 }
+
 export default App;
