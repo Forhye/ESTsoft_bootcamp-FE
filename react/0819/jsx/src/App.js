9f6a65682dd7b709ca848341b6dd4b86a171740a
Forhye <onlyh4425@gmail.com>
2024-08-19 10:38:09 +0900

    ✅ feat : 버튼 클릭시 like 토글

--- react/0819/jsx/src/App.js
+++ react/0819/jsx/src/App.js
@@ -1,8 +1,24 @@
+import { useState } from "react";
+
 function App() {
+  const [like, setLike] = useState(0);
+
+  function clickLike() {
+    if (like === "") {
+      setLike("Like");
+    } else {
+      setLike("");
+    }
+  }
+
+  // 위의 코드와 같다
+  // setLike(like ? "" : "Like");
+
   return (
-    <div>
-      hello world
+    <div style={{ border: "solid 1px", width: "500px" }}>
+      <button onClick={clickLike}>like</button> <span>{like}</span>
     </div>
   );
 }
+
 export default App;
