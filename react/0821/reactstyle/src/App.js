6be6ac9fe5b3fefa947baeee5659e2b3a40563d3
Forhye <onlyh4425@gmail.com>
2024-08-21 17:14:52 +0900

    ✅ add : 글로벌 스타일링 적용 (리셋, 노멀라이즈)

---
+++ react/0821/reactstyle/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Question from "./Question";
+import styles from "./App.module.css";
+import { createGlobalStyle } from "styled-components";
+// import reset from "styled-components";
+import Normalize from "styled-normalize";
+
+const GlobalStyle = createGlobalStyle`
+${Normalize}
+
+span{
+  color: darkolivegreen;
+  font-size: 2rem;
+}
+`;
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <span>글로벌 스타일 적용</span>
+      <nav className={styles.box}>
+        <ul>
+          <li id="detail" className={styles.text}>
+            상세정보
+          </li>
+          <li id="qa" className={styles.text}>
+            Q&A
+          </li>
+          <li id="review" className={styles.text}>
+            Review
+          </li>
+        </ul>
+      </nav>
+      <Question />
+    </>
+  );
+};
+
+export default App;
