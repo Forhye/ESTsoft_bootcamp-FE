2674af5fc10ca85eb7b84980d2696baf76ca7e17
Forhye <onlyh4425@gmail.com>
2024-08-22 16:04:58 +0900

    ✅ 실습 : 합성컨포넌트- children으로 전달해서 렌더링하기

---
+++ react/0822/component_fusion/src/App2.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function App2() {
+  const ProductImage = ({ src, alt }) => {
+    return <img src={src} alt={alt}></img>;
+  };
+
+  const ProductTitle = ({ children }) => <h2>{children}</h2>;
+
+  const ProductSubTitle = ({ children }) => <h3>{children}</h3>;
+
+  const ProductDescription = ({ children }) => <p>{children}</p>;
+
+  const ProductPrice = (props) => {
+    return (
+      <p>
+        가격 : {props.price}
+        {props.currencyCode}
+      </p>
+    );
+  };
+
+  const ContCard = ({ children }) => (
+    <div className="cont-card">{children}</div>
+  );
+
+  return (
+    <ContCard>
+      <ProductImage
+        src={"https://picsum.photos/id/237/200/300"}
+        alt={"검은강아지"}
+      />
+      <ProductTitle>검은 강아지를 놀아줄 장난감을 사세요!</ProductTitle>
+      <ProductSubTitle>핸드볼_검은 강아지용</ProductSubTitle>
+      <ProductDescription>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+        distinctio sed molestias saepe reiciendis fugit illum enim et inventore,
+        aliquam esse non nam consectetur minima atque consequuntur voluptates,
+        eum quia.
+      </ProductDescription>
+      <ProductPrice price={1000} currencyCode={"KRW"} />
+    </ContCard>
+  );
+}
+
+export default App2;
