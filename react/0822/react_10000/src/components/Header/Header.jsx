3957241fc1af433e80c8dc5f56914f63d6153d61
Forhye <onlyh4425@gmail.com>
2024-08-22 11:17:25 +0900

    ✅ update : header 모듈 css로 수정 + 클래스명 수정

--- react/0822/react_10000/src/components/Header/Header.jsx
+++ react/0822/react_10000/src/components/Header/Header.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import title from "../../images-law/title.png";
 import subtitle from "../../images-law/txt_subtitle.png";
-import "./Header.css";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
     <header>
-      <h1 className="tit-law">
-        <img src={title} alt="1만 시간의 법칙" className="img-tit" />
+      <h1 className={styles["tit-law"]}>
+        <img src={title} alt="1만 시간의 법칙" className={styles["img-tit"]} />
       </h1>
       <img
         src={subtitle}
         alt="연습은 어제의 당신보다 당신을 더 낫게 만든다."
-        className="img-txt"
+        className={styles["img-txt"]}
       />
-      <p className="txt-desc">
-        <strong className="tit-desc">1만 시간의 법칙</strong>은<br />
+      <p className={styles["txt-desc"]}>
+        <strong className={styles["tit-desc"]}>1만 시간의 법칙</strong>은<br />
         어떤 분야의 전문가가 되기 위해서는
         <br />
         최소한 1만 시간의 훈련이 필요하다는 법칙이다.
