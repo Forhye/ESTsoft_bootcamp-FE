9c797dc09b1c95efe6de696219d9db49e5d79849
Forhye <onlyh4425@gmail.com>
2024-08-22 13:53:17 +0900

    ✅ update : 버튼을 누르면 모달이 나타나는 기능 + className을 따로 추가해주는 방법 + 이후 Ref와 Effect로 수정

--- react/0822/react_10000/src/App.js
+++ react/0822/react_10000/src/App.js
@@ -3,14 +3,25 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import Modal from "./components/Modal/Modal";
+import { useState } from "react";
 
 function App() {
+  const [modalShow, setModalShow] = useState(false);
+
+  const modalOpen = () => {
+    setModalShow(true);
+  };
+
+  const modalClose = () => {
+    setModalShow(false);
+  };
+
   return (
     <div id="app">
       <Header />
-      <Main />
+      <Main modalOpen={modalOpen} />
       <Footer />
-      <Modal />
+      <Modal modalClose={modalClose} modalShow={modalShow} />
     </div>
   );
 }
--- react/0822/react_10000/src/components/Modal/Modal.jsx
+++ react/0822/react_10000/src/components/Modal/Modal.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import licat from "../../images-law/licat.png";
 import "./Modal.css";
 
-export default function Modal() {
+export default function Modal({ modalShow, modalClose }) {
+  // 돔 요소에 리액트가 접근하고 싶을 때
+  const dialogRef = useRef();
+
+  // 컴포넌트가 마운트 된 후에 실행
+  useEffect(() => {
+    if (modalShow) {
+      dialogRef.current.showModal();
+    } else {
+      dialogRef.current.close();
+    }
+  }, [modalShow]);
+
   return (
-    <dialog id="modal">
+    <dialog id="modal" ref={dialogRef}>
       <form className="modal-wrap" method="dialog">
         <h2>화이팅!!♥♥♥</h2>
         <h3>당신의 꿈을 응원합니다!</h3>
         <img src={licat} alt="라이캣의 응원" />
-        <button className="btn-close">
+        <button className="btn-close" onClick={modalClose}>
           종료하고 진짜 훈련하러 가기 GO!GO!
         </button>
         <p>(그냥 닫기 버튼입니다.)</p>
