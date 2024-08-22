22bbd13504cc9e8c150dcd9c2c1c8a9fef81af5b
Forhye <onlyh4425@gmail.com>
2024-08-22 10:45:06 +0900

    ✅ add : Modal 영역 리액트화

--- react/0822/react_10000/src/App.js
+++ react/0822/react_10000/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import Modal from "./components/Modal/Modal";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Main />
       <Footer />
+      <Modal />
     </div>
   );
 }
