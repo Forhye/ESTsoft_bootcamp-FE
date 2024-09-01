db1ce6b8e16b832cb2f78f824607e88effda7e45
Forhye <onlyh4425@gmail.com>
2024-09-01 23:26:35 +0900

    ✅ add : 수량 +- 렌더함수

---
+++ react/0830/react-redux/src/modules/goodsCounter.js
@@ -0,0 +1,25 @@
+export const addNumber = () => {
+  return { type: "ADD" };
+};
+
+export const minuseNumber = () => {
+  return { type: "MINUS" };
+};
+
+const initialState = {
+  stock: 5,
+  goods: 1,
+};
+
+const goodsReucer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD":
+      return { ...state, stock: state.stock - 1, goods: state.goods + 1 };
+    case "MINUS":
+      return { ...state, stock: state.stock + 1, goods: state.goods - 1 };
+    default:
+      return state;
+  }
+};
+
+export default goodsReucer;
